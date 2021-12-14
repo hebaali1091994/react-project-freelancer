ffc8ba0169c52757079d97bc0cab06f86107c825
abobakerhanfy <abobakerhanfy2019@gmail.com>
2021-12-14 18:05:35 +0200

    last

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@
 import './App.css';
 import Home from './Pages/Home';
 import Postproject from './Pages/post-project/Postproject.jsx';
-import Login from './Components/Login/Login.jsx';
+import Login from './Pages/Login/Login';
 import { Routes, Route } from "react-router";
 import SignUp from "./Components/Sign up/SignUp";
 import Paypal from "./Components/Paypal/paypal";
-import { Suspense, useState } from "react";
+import { Suspense, useState, useContext } from "react";
 
 import '../src/i18n'
+import { Context } from './context/Context';
+import Dashbaord from './Pages/Dashbaord/Dashbaord';
 
 function App() {
+      const {user} = useContext(Context);
+
   const lang = localStorage.getItem('lang') || 'en';
   document.documentElement.language = lang;
   return (
@@ -22,6 +26,8 @@ function App() {
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path='/paypal'  element={<Paypal  />}/>
+                <Route path='/Dashbaord'  element={<Dashbaord  />}/>
+
       </Routes>
     </div>
     </Suspense>
